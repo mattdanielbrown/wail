27019e8576b160e1d2b229eb58020a9ed6016e30
N0taN3rd <n0tan3rd@gmail.com>
2017-02-10 00:05:07 -0500

    bump version

--- build.js
+++ build.js
@@ -4,12 +4,13 @@ const fs = require('fs-extra')
 
 const here = process.cwd()
 
+const version = '1.0.2'
 const wailHomeOpts = {
   youtubelink: 'https://www.youtube.com/embed/xAovJNO08OI',
   wikilink: 'https://github.com/N0taN3rd/wail/wiki',
-  macosdl: 'https://github.com/N0taN3rd/wail/releases/download/1.0.0/WAIL.dmg',
-  linuxdl: 'https://github.com/N0taN3rd/wail/releases/download/1.0.0/WAIL-linux-x64-build-u16.04.zip',
-  windowsdl: 'https://github.com/N0taN3rd/wail/releases/download/1.0.0/WAIL-win32-x64.zip',
+  macosdl: `https://github.com/N0taN3rd/wail/releases/download/${version}/WAIL.dmg`,
+  linuxdl: `https://github.com/N0taN3rd/wail/releases/download/${version}/WAIL-linux-x64.zip`,
+  windowsdl: `https://github.com/N0taN3rd/wail/releases/download/${version}/WAIL-win32-x64.zip`,
   projectHome: 'https://github.com/N0taN3rd/wail',
 }
 
